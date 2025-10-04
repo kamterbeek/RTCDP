56eff7ed8690353030718b1ba07295d6671a121b
Kassandra <kam.terbeek@gmail.com>
2025-10-03 21:09:18 -1000

    Update CDGenerator.py

--- CDGenerator.py
+++ CDGenerator.py
@@ -7,4 +7,9 @@ users ["email", "email"]
 
 for i in range(5):
   event = {
-    
+    "eventType": random.choice(events),
+    "userId": random.choice(users),
+    "timestampe": time.time()
+  }
+
+print(json.dumps(events))
